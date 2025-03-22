28a689e902894aabd7f059e965120cfe54310b16
oninonins <varelinobubadibako11@gmail.com>
2025-03-22 21:24:28 +0700

    baru

---
+++ MahasiswaBerprestasi05.java
@@ -0,0 +1,36 @@
+public class MahasiswaBerprestasi05 {
+    Mahasiswa05 [] listMhs = new Mahasiswa05[5];
+    int idx;
+
+
+    void tambah(Mahasiswa05 m){
+        if (idx < listMhs.length){
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh");
+        }
+    }
+
+
+    void tampil(){
+        for (Mahasiswa05 m : listMhs){
+            m.tampilInformasi();
+            System.out.println("-------------------------------");
+        }
+    }
+
+    void bubbleSort(){
+        for (int i=0; i<listMhs.length-1; i++){
+            for (int j=1; j<listMhs.length-i; j++){
+                if (listMhs[j].ipk < listMhs[j-1].ipk){
+                    //proses pertukaran
+                    Mahasiswa05 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = tmp;
+                }
+            }
+        }
+    }
+}
+
